3ecb33a3bd134fd1c84d6df898da9ebfdaccc069
Nane Kratzke <nane.kratzke@googlemail.com>
2014-12-12 08:36:07 +0100

    Reset

--- assets/webtech/examples/JavaScript/EventHandler/handler.js
+++
@@ -1,8 +0,0 @@
-var over = 0;
-
-function mouseover() {
-	var text = document.createTextNode("Ihr " + ++over + " Mouse over! ");
-	document.getElementById("miracle").appendChild(text);
-	br = document.createElement("br");
-	document.getElementById("miracle").appendChild(br);
-}
--- assets/webtech/examples/JavaScript/HelloWorld/hw.js
+++
@@ -1,2 +0,0 @@
-document.write("Hello World! ");
-document.write("Dies ist durch ein eingebundenes Skript clientseitig erstellt worden.");
